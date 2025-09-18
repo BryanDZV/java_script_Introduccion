4d09073cf40775cfd814f3264573922a4624d262
Bryan <bryan.dweb@gmail.com>
2025-09-18 22:21:11 +0200

    fin ejercicio 1

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,5 +1,39 @@
-let x=[5,56,323,];
+/*CREA UNA FUNCION QUE RECIBIA UN ARRAY CON CADENA DE CARACTERES Y QUE DEVUELVA 
+LAS QUE TENGAN UNA LONGITUD MAYOR A 3
+CREAR OTRA FUNCION QUE RECIBIA NUMEROS Y QUE DEVUELVE LOS PARES
+CREAR OTRA FUNCION QUE DEVUELVA LOS MENORES E IGUALES QUE 3*/
 
-let [valores]=x;
+const palabras = ["sol", "luna", "mar", "cielo"];
+const numeros = [1, 2, 3, 4, 5, 6];
 
-console.log(valores);
+//USANDO FILTER NATIVO
+
+let maestra=palabras.filter(f);
+let num=numeros.filter(fx);
+let menores=numeros.filter(fj);
+
+//logicas funciones definas
+function f(p){
+    if (p.length>3) {
+        return p;
+    }
+};
+function fx(n){
+    if (n%2==0) {
+        return n ;
+    }
+    
+}
+
+function fj(n){
+    if (n<=3) {
+        return n;
+    }
+}
+
+//resultado
+console.log(maestra);
+console.log(num);
+console.log(menores);
+
+
--- introduccion.js
+++ introduccion.js
@@ -5,3 +5,12 @@ var m=4;
 
 console.log(f);
 console.log(n+m);
+
+//el typeOf te dice  el tipo de dato de una variable,
+
+var r=4;
+console.log(typeof(r));
+
+var x="4";
+
+console.log(x===r);
